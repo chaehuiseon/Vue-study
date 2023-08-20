47e8a71ab49898d4a183e1909135bad0b5aae84b
chaehuiseon <smilechs34@gmail.com>
2023-08-20 20:23:55 +0900

    2

--- vue-study/vue/test2/src/components/InputField.vue
+++ vue-study/vue/test2/src/components/InputField.vue
@@ -5,7 +5,8 @@
             type = "text"
             :value="name"
             style="padding: 30px; border: 2px solid green;"
-            @input="updateName"> <!--유저가 인풋해서 값이 바뀌었을때, 부모 컨포넌트에게 보내기 위함. 인풋이벤트 발동-->
+            @input="$emit('update-name',$event)">
+            <!--@input="updateName">유저가 인풋해서 값이 바뀌었을때, 부모 컨포넌트에게 보내기 위함. 인풋이벤트 발동-->
     </div>
             
 </template>
@@ -19,12 +20,12 @@ export default{
             required: true
         }
     },
-    methods:{
-        updateName(e){
-            console.log(e.target.value);
-            //부모 컨포넌트에게 값을 올림
-            this.$emit('update-name',e.taget.value);
-        }
-    }
+    // methods:{
+    //     updateName(e){
+    //         console.log(e.target.value);
+    //         //부모 컨포넌트에게 값을 올림
+    //         this.$emit('update-name',e.taget.value);
+    //     }
+    // }
 }
 </script>
--- vue-study/vue/test2/src/views/HomeView2.vue
+++ vue-study/vue/test2/src/views/HomeView2.vue
@@ -4,8 +4,7 @@
         <form action = "">
             <!--(1)props로 데이터를 넘겨주고 (2)InputField에서 value 값이 변하면 emit으로 부모 컴포넌트에게 값(e.target.value)을 보내줘서
             (3)부모 컴포넌트에서 update-name 이벤트로 보내온 값을 받아와서 아래 name 업데이트 시켜준다.-->
-            <InputField :name="name" @updaate-name="updateName"/>
-
+            <InputField :name="name" @update-name="name = $event.target.value"/>
             <br><button>Submit</button>
         </form>
         {{name}}
@@ -23,15 +22,15 @@ export default {
 
     data(){
         return {
-            name : "",
+            name : ""
         }
     },
-    methods:{
-        //이벤트로 받으면 함수를 실행시킴. 넘어온 value(name)을 부모의 name에 업데이트를 시켜준다. ㅇ
-        updateName(name){
-            this.name = name;
-        }
-    }
+    // methods:{
+    //     //이벤트로 받으면 함수를 실행시킴. 넘어온 value(name)을 부모의 name에 업데이트를 시켜준다. ㅇ
+    //     updateName(name){
+    //         this.name = name;
+    //     }
+    // }
    
 
 }
